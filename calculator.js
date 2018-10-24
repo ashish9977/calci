a4bed28fda26c57df51e87fe85e6d8b6b955e4b0
ashish9977 <43572273+ashish9977@users.noreply.github.com>
2018-10-24 21:32:34 +0530

    calci

---
+++ calculator.js
@@ -0,0 +1,82 @@
+function insert(num)
+{
+document.form.text.value += num;	
+}
+function equal ()
+{var exp=document.form.text.value;
+	if(exp){
+
+document.form.text.value=eval(exp);
+}}
+function clean()
+{
+
+document.form.text.value="";
+
+
+
+
+}
+
+function power(){
+
+document.form.text.value="CALCULATOR ON";
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
